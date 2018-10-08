7247d78586ef3f5d76ac045697e86cb335672e8f
Cristian <cristian.rock78@gmail.com>
2018-10-07 21:12:33 -0300

    Primer commit Construccion de escape v0.0.1

---
+++ Salida/Source/Salida/SalidaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SalidaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SALIDA_API ASalidaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
